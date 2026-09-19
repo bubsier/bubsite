30b0814c79c1824164d6cee7fe5267180da12d37
bubsier <benjaminvolheim@gmail.com>
2026-09-19 14:56:15 -0400

    Add files via upload

---
+++ visited.js
@@ -0,0 +1,17 @@
+const visitedCountries = [
+    "United States",
+    "France",
+    "Denmark",
+    "Italy",
+    "Spain",
+    "Japan",
+    "Morocco",
+    "Greece",
+    "Hungary",
+    "Sweden",
+    "Switzerland",
+    "United Kingdom",
+    "Vatican",
+    "Costa Rica",
+    "Nicaragua"
+];
